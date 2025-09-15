b355cbd67f20e8268d1e79534249ae5ced119cc7
redehe <122535081+redehe@users.noreply.github.com>
2025-09-15 13:17:45 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+
+btn.addEventListener("click", () => {
+  msg.textContent = "🎉 恭喜！你的 GitHub Pages 网站已生效！";
+});
